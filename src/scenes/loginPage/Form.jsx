20f517f52505772cf14e90a1fc69d690e4c846a9
duclairdeugoue <duclairdeugoue@gmail.com>
2023-05-23 02:34:07 +0100

    resumming project

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import {
-  Box,
-  Button,
-  TextField,
-  useMediaQuery,
-  Typograpy,
-  useTheme,
+    Box,
+    Button,
+    TextField,
+    useMediaQuery,
+    Typograpy,
+    useTheme,
 } from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
@@ -17,79 +17,80 @@ import Dropzone from "react-dropzone";
 import FlexBetween from "components/FlexBetween";
 
 const registerSchema = yup.object().shape({
-  firstName: yup.string().required("required"),
-  lastName: yup.string().required("required"),
-  email: yup.string().email("invalid email").required("required"),
-  password: yup.string().required("required"),
-  location: yup.string().required("required"),
-  occupation: yup.string().required("required"),
-  picture: yup.string().required("required"),
+    firstName: yup.string().required("required"),
+    lastName: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required"),
+    location: yup.string().required("required"),
+    occupation: yup.string().required("required"),
+    picture: yup.string().required("required"),
 });
 
 const loginSchema = yup.object().shape({
-  email: yup.string().email("invalid email").required("required"),
-  password: yup.string().required("required"),
+    email: yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required"),
 });
 
+
 const initialValueRegister = {
-  firstName: "",
-  lastName: "",
-  email: "",
-  password: "",
-  location: "",
-  occupation: "",
-  picture: "",
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    location: "",
+    occupation: "",
+    picture: "",
 };
 
 const initialValueLogin = {
-  email: "",
-  password: "",
+    email: "",
+    password: "",
 };
 
 
 const Form = () => {
-  const [pageType, setPageType] = useState("login");
-  const { pallete } = useTheme();
-  const { dispatch } = useDispatch();
-  const navigate = useNavigate();
-  const isNonMobile = useMediaQuery("min-width: 600px");
-  const isLogin = pageType === "login";
-  const isRegister = pageType === "register";
+    const [pageType, setPageType] = useState("login");
+    const { pallete } = useTheme();
+    const { dispatch } = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("min-width: 600px");
+    const isLogin = pageType === "login";
+    const isRegister = pageType === "register";
 
-  const handlerFormSubmit = async (values, onSubmitPage) => {
+    const handlerFormSubmit = async (values, onSubmitPage) => {
 
-  };
-  return (
-    <Formik
-      onSubmit={handlerFormSubmit}
-      initialValues={isLogin ? initialValueLogin : initialValueRegister}
-      validationSchema={isLogin ? loginSchema : registerSchema}>
-      {({
-        values,
-        errors,
-        touched,
-        handleBlur,
-        handleChange,
-        handleSubmit,
-        setFieldValue,
-        resetForm,
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <Box
-            display="grid"
-            gap="30px"
-            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
-            sx={{
-              "& > div": {
-                gridColumn: isNonMobile ? undefined : "span 4",
-              }
-            }}>
-            {/* {isRegister && ()} */}
-          </Box>
-        </form>
-      )}
-    </Formik>
-  )
+    };
+    return (
+        <Formik
+            onSubmit={handlerFormSubmit}
+            initialValues={isLogin ? initialValueLogin : initialValueRegister}
+            validationSchema={isLogin ? loginSchema : registerSchema}>
+            {({
+                values,
+                errors,
+                touched,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                setFieldValue,
+                resetForm,
+            }) => (
+                <form onSubmit={handleSubmit}>
+                    <Box
+                        display="grid"
+                        gap="30px"
+                        gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                        sx={{
+                            "& > div": {
+                                gridColumn: isNonMobile ? undefined : "span 4",
+                            }
+                        }}>
+                        {/* {isRegister && ()} */}
+                    </Box>
+                </form>
+            )}
+        </Formik>
+    )
 }
 
 export default Form
