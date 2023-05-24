d93744ca8d7cd7be9cd9cdec931f93528861747f
duclairdeugoue <duclairdeugoue@gmail.com>
2023-05-24 19:26:09 +0100

    pages: adding the login and register page

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,21 +1,48 @@
-import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import { useState } from 'react';
+import {
+    Box,
+    Button,
+    TextField,
+    Typography,
+    useTheme,
+    useMediaQuery
+} from '@mui/material';
+import { Formik } from 'formik';
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import FlexBetween from "components/FlexBetween";
+
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email("invalid email").required("required"),
+    password: yup.string().required("required"),
+});
+
+const initialValueLogin = {
+    email: "",
+    password: "",
+};
+
+const handleLoginFormSubmit = async (req, res) => { };
 
 const Index = () => {
-    const theme = useTheme();
+    const { palette } = useTheme();
+    const { dispatch } = useDispatch();
+    const navigate = useNavigate();
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
     return (
         <Box>
             <Box
                 width="100%"
-                backgroundColor={theme.palette.background.alt}
+                backgroundColor={palette.background.alt}
                 p="1rem 6%"
-                textAlign="center"
-            >
+                textAlign="center">
                 <Typography
                     fontWeight="Bold"
                     fontSize="clamp(1rem, 2rem, 2.25rem)"
-                    color="primary"
-                >
+                    color="primary">
                     Sociopedia
                 </Typography>
             </Box>
@@ -24,12 +51,89 @@ const Index = () => {
                 p="2rem"
                 m="2rem auto"
                 borderRadius="1.5rem"
-                backgroundColor={theme.palette.background.alt}
-            >
+                backgroundColor={palette.background.alt}>
                 <Typography fontWeight="500" variant='h5' sx={{ mb: "1.5rem" }}>
                     Welcome to Sociopedia, the Social Media for Sociopaths!
                 </Typography>
+                <Formik
+                    onSubmit={handleLoginFormSubmit}
+                    initialValues={initialValueLogin}
+                    validationSchema={loginSchema}>
+                    {
+                        ({
+                            values,
+                            errors,
+                            touched,
+                            handleBlur,
+                            handleChange,
+                            handleSubmit,
+                            setFieldValue,
+                            resetForm
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                <Box
+                                    display="grid"
+                                    gap="30px"
+                                    gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                                    sx={{
+                                        "& > div": {
+                                            gridColumn: isNonMobileScreens ? undefined : "span 4"
+                                        }
+                                    }}>
+                                    <TextField
+                                        label="Email"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        value={values.email}
+                                        name="email"
+                                        error={Boolean(touched.email) && Boolean(errors.email)}
+                                        helperText={touched.email && errors.email}
+                                        sx={{ gridColumn: "span 4" }} />
+                                    <TextField
+                                        label="Password"
+                                        type="password"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        value={values.password}
+                                        name="password"
+                                        error={Boolean(touched.password) && Boolean(errors.password)}
+                                        helperText={touched.password && errors.password}
+                                        sx={{ gridColumn: "span 4" }} />
+                                </Box>
+                                <Box>
+                                    <Button
+                                        fullWidth
+                                        type="submit"
+                                        sx={{
+                                            m: "2rem 0",
+                                            p: "1rem",
+                                            backgroundColor: palette.primary.main,
+                                            color: palette.background.alt,
+                                            "&:hover": { color: palette.primary.dark }
+                                        }}>
+                                        LOGIN
+                                    </Button>
+                                    <Typography
+                                        onClick={() => navigate("/register")}
+                                        sx={{
+                                            textDecoration: "underline",
+                                            color: palette.primary.main,
+                                            "&:hover": {
+                                                cursor: "pointer",
+                                                color: palette.primary.dark,
+                                            }
+                                        }}>
+                                        Don't have an account? Sign Up here.
+                                    </Typography>
+                                </Box>
+                            </form>
+                        )
+                    }
+
+                </Formik>
             </Box>
+
+
         </Box>
     )
 }
