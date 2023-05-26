f224c1f6846b2a36f9aaabfa7481f374936bebc5
duclairdeugoue <duclairdeugoue@gmail.com>
2023-05-26 19:04:09 +0100

    [fixed]: refactor App component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import {
-  LandingPage,
-  LoginPage,
-  RegisterPage,
-  HomePage,
-  ProfilePage,
+    LandingPage,
+    LoginPage,
+    RegisterPage,
+    HomePage,
+    ProfilePage,
 } from 'pages';
 import { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { createTheme } from '@mui/material/styles';
 import { themeSettings } from 'theme';
-import ProtectedRoute from 'components/molecules/protected-route/ProtectedRoute';
+import { ProtectRouteComponent } from 'components';
 
 function App() {
-  const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/home" element={<ProtectedRoute><HomePage /></ProtectedRoute>} />
-          <Route path="/profile/:userId" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>} />
-        </Routes>
-      </ThemeProvider>
+    const mode = useSelector((state) => state.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return (
+        <BrowserRouter>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Routes>
+                    <Route
+                        path="/"
+                        element={<LandingPage />}
+                    />
+                    <Route
+                        path="/login"
+                        element={<LoginPage />}
+                    />
+                    <Route
+                        path="/register"
+                        element={<RegisterPage />}
+                    />
+                    <Route
+                        path="/home"
+                        element={<ProtectRouteComponent><HomePage /></ProtectRouteComponent>}
+                    />
+                    <Route
+                        path="/profile/:userId"
+                        element={<ProtectRouteComponent><ProfilePage /></ProtectRouteComponent>}
+                    />
+                </Routes>
+            </ThemeProvider>
 
-    </BrowserRouter>
-  );
+        </BrowserRouter>
+    );
 }
 
 export default App;
