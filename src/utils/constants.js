d14a04a52ad63bb6dd171f6447404d47d2896cc7
duclairdeugoue <duclairdeugoue@gmail.com>
2023-05-30 17:58:40 +0100

    testing: rowback the api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'http://localhost:3001';
-export const API_URL = 'https://sociopedia-clone0-be.vercel.app';
+export const API_URL = 'http://localhost:3001';
+// export const API_URL = 'https://sociopedia-clone0-be.vercel.app';
