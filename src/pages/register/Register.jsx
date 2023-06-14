3a298b97d1ba4f2acb08acfb6271fefe4b96bd4c
duclairdeugoue <duclairdeugoue@gmail.com>
2023-06-14 15:28:23 +0100

    review updates

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -4,15 +4,15 @@ import {
   TextField,
   Typography,
   useTheme,
-  useMediaQuery
-} from '@mui/material';
+  useMediaQuery,
+} from "@mui/material";
 import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
-import { Formik } from 'formik';
+import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
 import Dropzone from "react-dropzone";
 import { FlexBetweenComponent } from "components";
-import { AuthService } from 'services';
+import { AuthService } from "services";
 
 const registerSchema = yup.object().shape({
   firstName: yup.string().required("required"),
@@ -24,7 +24,6 @@ const registerSchema = yup.object().shape({
   picture: yup.string().required("required"),
 });
 
-
 const initialValueRegister = {
   firstName: "",
   lastName: "",
@@ -34,7 +33,6 @@ const initialValueRegister = {
   occupation: "",
   picture: "",
 };
-
 
 const Register = () => {
   const { palette } = useTheme();
@@ -47,16 +45,20 @@ const Register = () => {
     for (let value in values) {
       formData.append(value, values[value]);
     }
-    formData.append('picturePath', values.picture.name);
+    console.log(formData);
+    formData.append("picturePath", values.picture.name);
 
-    AuthService.register(formData).then((res) => {
-      if (res) {
-        onSubmitProps.resetForm();
-        navigate('/login');
-      }
-    }).catch((err) => { console.log(err); });
-
-  }
+    AuthService.register(formData)
+      .then((res) => {
+        if (res) {
+          onSubmitProps.resetForm();
+          navigate("/login");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   return (
     <Box>
@@ -64,11 +66,13 @@ const Register = () => {
         width="100%"
         backgroundColor={palette.background.alt}
         p="1rem 6%"
-        textAlign="center">
+        textAlign="center"
+      >
         <Typography
           fontWeight="Bold"
           fontSize="clamp(1rem, 2rem, 2.25rem)"
-          color="primary">
+          color="primary"
+        >
           Sociopedia
         </Typography>
       </Box>
@@ -77,14 +81,16 @@ const Register = () => {
         p="2rem"
         m="2rem auto"
         borderRadius="1.5rem"
-        backgroundColor={palette.background.alt}>
-        <Typography fontWeight="500" variant='h5' sx={{ mb: "1.5rem" }}>
+        backgroundColor={palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
           Welcome to Sociopedia, the Social Media for Sociopaths!
         </Typography>
         <Formik
           onSubmit={handleRegisterFormSubmit}
           initialValues={initialValueRegister}
-          validationSchema={registerSchema}>
+          validationSchema={registerSchema}
+        >
           {({
             values,
             errors,
@@ -103,8 +109,9 @@ const Register = () => {
                 sx={{
                   "& > div": {
                     gridColumn: isNonMobileScreens ? undefined : "span 4",
-                  }
-                }}>
+                  },
+                }}
+              >
                 <>
                   <TextField
                     label="First Name"
@@ -112,67 +119,77 @@ const Register = () => {
                     onChange={handleChange}
                     value={values.firstName}
                     name="firstName"
-                    error={Boolean(touched.firstName) && Boolean(errors.firstName)}
+                    error={
+                      Boolean(touched.firstName) && Boolean(errors.firstName)
+                    }
                     helperText={touched.firstName && errors.firstName}
-                    sx={{ gridColumn: "span 2" }} />
+                    sx={{ gridColumn: "span 2" }}
+                  />
                   <TextField
                     label="Last Name"
                     onBlur={handleBlur}
                     onChange={handleChange}
                     value={values.lastName}
                     name="lastName"
-                    error={Boolean(touched.lastName) && Boolean(errors.lastName)}
+                    error={
+                      Boolean(touched.lastName) && Boolean(errors.lastName)
+                    }
                     helperText={touched.lastName && errors.lastName}
-                    sx={{ gridColumn: "span 2" }} />
+                    sx={{ gridColumn: "span 2" }}
+                  />
                   <TextField
                     label="Location"
                     onBlur={handleBlur}
                     onChange={handleChange}
                     value={values.location}
                     name="location"
-                    error={Boolean(touched.location) && Boolean(errors.location)}
+                    error={
+                      Boolean(touched.location) && Boolean(errors.location)
+                    }
                     helperText={touched.location && errors.location}
-                    sx={{ gridColumn: "span 4" }} />
+                    sx={{ gridColumn: "span 4" }}
+                  />
                   <TextField
                     label="Occupation"
                     onBlur={handleBlur}
                     onChange={handleChange}
                     value={values.occupation}
                     name="occupation"
-                    error={Boolean(touched.occupation) && Boolean(errors.occupation)}
+                    error={
+                      Boolean(touched.occupation) && Boolean(errors.occupation)
+                    }
                     helperText={touched.occupation && errors.occupation}
-                    sx={{ gridColumn: "span 4" }} />
+                    sx={{ gridColumn: "span 4" }}
+                  />
                   <Box
                     gridColumn="span 4"
                     border={`1px solid ${palette.neutral.medium}`}
                     borderRadius="5px"
-                    p="1rem">
+                    p="1rem"
+                  >
                     <Dropzone
                       acceptedFiles=".jpg,.jpeg,.png"
                       multiple={false}
                       onDrop={(acceptedFiles) => {
-                        setFieldValue("picture", acceptedFiles[0])
-                      }}>
+                        setFieldValue("picture", acceptedFiles[0]);
+                      }}
+                    >
                       {({ getRootProps, getInputProps }) => (
                         <Box
                           {...getRootProps()}
                           border={`2px dashed ${palette.primary.main}`}
                           p="2rem"
-                          sx={{ "&:hover": { cursor: "pointer" } }}>
+                          sx={{ "&:hover": { cursor: "pointer" } }}
+                        >
                           <input {...getInputProps()} />
-                          {!values.picture ?
-                            (
-                              <p>Click here to add a picture</p>
-                            ) : (
-                              <FlexBetweenComponent>
-                                <Typography>
-                                  {values.picture.name}
-                                </Typography>
-                                <EditOutlinedIcon />
-                              </FlexBetweenComponent>
-                            )
-                          }
-
+                          {!values.picture ? (
+                            <p>Click here to add a picture</p>
+                          ) : (
+                            <FlexBetweenComponent>
+                              <Typography>{values.picture.name}</Typography>
+                              <EditOutlinedIcon />
+                            </FlexBetweenComponent>
+                          )}
                         </Box>
                       )}
                     </Dropzone>
@@ -186,7 +203,8 @@ const Register = () => {
                   name="email"
                   error={Boolean(touched.email) && Boolean(errors.email)}
                   helperText={touched.email && errors.email}
-                  sx={{ gridColumn: "span 4" }} />
+                  sx={{ gridColumn: "span 4" }}
+                />
                 <TextField
                   label="Password"
                   type="password"
@@ -196,7 +214,8 @@ const Register = () => {
                   name="password"
                   error={Boolean(touched.password) && Boolean(errors.password)}
                   helperText={touched.password && errors.password}
-                  sx={{ gridColumn: "span 4" }} />
+                  sx={{ gridColumn: "span 4" }}
+                />
               </Box>
               {/* LOGIN AND REGISTER BUTTON */}
               <Box>
@@ -208,8 +227,9 @@ const Register = () => {
                     p: "1rem",
                     backgroundColor: palette.primary.main,
                     color: palette.background.alt,
-                    "&:hover": { color: palette.primary.dark }
-                  }}>
+                    "&:hover": { color: palette.primary.dark },
+                  }}
+                >
                   REGISTER
                 </Button>
                 <Typography
@@ -220,8 +240,9 @@ const Register = () => {
                     "&:hover": {
                       cursor: "pointer",
                       color: palette.primary.dark,
-                    }
-                  }}>
+                    },
+                  }}
+                >
                   Already have an account? Login here.
                 </Typography>
               </Box>
@@ -229,11 +250,8 @@ const Register = () => {
           )}
         </Formik>
       </Box>
-
-
     </Box>
-
   );
-}
+};
 
 export default Register;
